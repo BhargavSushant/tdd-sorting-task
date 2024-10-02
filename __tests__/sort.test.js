10583bff1c9cddfd6cbc67dd106d03c1440d83f0
BhargavSushant <BhargavSushant@gmail.com>
2024-10-02 10:06:07 +0530

    Add tests for edge cases

--- __tests__/sort.test.js
+++ __tests__/sort.test.js
@@ -5,3 +5,11 @@ test("sorts an array of numbers in ascending order", () => {
   const sortedArr = sort(arr);
   expect(sortedArr).toEqual([1, 3, 4, 5, 8]);
 });
+
+test("returns an empty array when given an empty array", () => {
+  expect(sort([])).toEqual([]);
+});
+
+test("handles arrays with duplicate values", () => {
+  expect(sort([3, 3, 1, 2, 2])).toEqual([1, 2, 2, 3, 3]);
+});
