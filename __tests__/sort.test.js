9e5ffb1abb1f1264e026314b47a176c9013c06e7
BhargavSushant <BhargavSushant@gmail.com>
2024-10-02 09:52:08 +0530

    Add initial test for sorting function

---
+++ __tests__/sort.test.js
@@ -0,0 +1,9 @@
+// __tests__/sort.test.js
+
+const sort = require("../sort");
+
+test("sorts an array of numbers in ascending order", () => {
+  const arr = [5, 3, 8, 4, 1];
+  const sortedArr = sort(arr);
+  expect(sortedArr).toEqual([1, 3, 4, 5, 8]);
+});
